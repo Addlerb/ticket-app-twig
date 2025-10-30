2c46dc6f8003e48177f19c033a5fadaf4ba63360
Addlerb <34371066+Addlerb@users.noreply.github.com>
2025-10-30 08:43:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
-$twig = new \Twig\Environment($loader);
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__ . '/cache',
+    'debug' => true,
+]);
 
-echo $twig->render('home.twig', ['name' => 'Friend']);
+$session = json_decode($_COOKIE['ticketapp_session'] ?? '{}', true);
+
+$route = $_SERVER['REQUEST_URI'];
+if (preg_match('/\/auth/', $route)) {
+    echo $twig->render('auth.twig', ['session' => $session]);
+} elseif (preg_match('/\/dashboard/', $route)) {
+    if (empty($session['token'])) {
+        header('Location: /auth/login');
+        exit;
+    }
+    echo $twig->render('dashboard.twig', ['stats' => getTicketStats()]);
+} else {
+    echo $twig->render('landing.twig', []);
+}
+?>
